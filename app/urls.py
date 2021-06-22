b739f2ba3c853b4c23b4d8ce8efb0b6a565c6fc5
Devanshi Jani <devanshijani169@gmail.com>
2021-06-22 20:06:34 +0530

    changes in base.html

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.home_view, name='index'),
+    path('home/', views.home_view, name='index'),
+    path('about/', views.about_view, name='about'),
+    path('contact/', views.contact_view, name='contact'),
 ]
