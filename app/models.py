fe627794e08adf68e993f11d772593099fe542e7
Devanshi Jani <devanshijani169@gmail.com>
2021-06-23 14:47:00 +0530

    changes in contact page

--- app/models.py
+++ app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+    message = models.CharField(max_length=250)
+    subject = models.TextField()
+
+    def __str__(self):
+        return self.name
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-
+from .models import Contact
+from django.contrib import messages
 # Create your views here.
 
 
@@ -11,5 +12,25 @@ def about_view(request):
     return render(request, "about.html")
 
 
-def contact_view(request):
-    return render(request, "contact.html")
+# def contact_view(request):
+#     return render(request, "contact.html")
+def contact_view(request, *args, **kwargs):
+
+    if request.method == 'POST':
+        name = request.POST['name']
+        email = request.POST['email']
+        subject = request.POST['subject']
+        message = request.POST['message']
+        if len(name) < 2 or len(email) < 3 or len(subject) < 4:
+            messages.error(request, "Please fill the form correctly.")
+
+        else:
+            if email.__contains__('.'):
+                contact = Contact(name=name, email=email,
+                                  subject=subject, message=message)
+                contact.save()
+                messages.success(
+                    request, "Your messages has been successfully sent.")
+            else:
+                messages.error(request, "Please Enter Correct Email Address.")
+    return render(request, "contact.html", {})
